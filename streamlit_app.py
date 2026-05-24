1e2c3528832dbb5c561d865011247df78c52d343
Vittoria Mariani <85867812+sjlentium@users.noreply.github.com>
2026-05-24 23:18:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,12 @@ st.set_page_config(
     page_title="Accident Severity Predictor",
     page_icon="🚗",
     layout="wide",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
 )
 
 # Mobile-friendly CSS
