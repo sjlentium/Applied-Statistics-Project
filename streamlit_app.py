a76ce1ea9718edbba0ac1d01c9b5907fb1c2aff1
Vittoria Mariani <85867812+sjlentium@users.noreply.github.com>
2026-05-25 08:39:46 +0200

    Change initial sidebar state to expanded

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ import sys
 st.set_page_config(
     page_title="Accident Severity Predictor",
     page_icon="🚗",
-    initial_sidebar_state="auto"
+    initial_sidebar_state="expanded"
 )
 
 with st.sidebar:
